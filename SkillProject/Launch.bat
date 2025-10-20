c33adf803b89c14c8bf99b31a9ff877c8bcf4827
chvj7567 <chvj7567@naver.com>
2025-10-20 23:31:10 +0900

    [Bat] 제너레이터 bat 파일 추가

---
+++ SkillProject/Launch.bat
@@ -0,0 +1,7 @@
+call "D:\UE_5.4\UE_5.4\Engine\Build\BatchFiles/Build.bat" ^
+-projectfiles -project="%~dp0\SkillProject.uproject" -game -rocket -progress
+
+call "D:\UE_5.4\UE_5.4\Engine\Build\BatchFiles\Build.bat" ^
+SkillProject Win64 Development -Project="%~dp0\SkillProject.uproject" -WaitMutex -FromMsBuild -architecture=x64
+
+call "%~dp0\SkillProject.uproject"
