09511c93d8aee68b4d692e71784e1c58e4f5dad2
chvj7567 <chvj7567@naver.com>
2025-12-03 23:25:11 +0900

    컴파일 시간 단축 매크로 추가

--- SkillProject/Source/SkillProject/Character/SpyCharacterMovementComponent.cpp
+++ SkillProject/Source/SkillProject/Character/SpyCharacterMovementComponent.cpp
@@ -5,6 +5,8 @@
 #include "Util/DefineEnum.h"
 #include "Character/SpyCharacter.h"
 
+#include UE_INLINE_GENERATED_CPP_BY_NAME(SpyCharacterMovementComponent)
+
 USpyCharacterMovementComponent::USpyCharacterMovementComponent()
 {
 }
