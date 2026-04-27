aeef5c7adadb70dc7a9db41a42e82486b0e02428
chvj7567 <chvj7567@naver.com>
2026-04-27 10:57:10 +0900

    [Data] SpyMovementConfig에 그래플링 훅 설정 필드 추가

--- SkillProject/Source/SkillProject/Data/SpyMovementConfig.h
+++ SkillProject/Source/SkillProject/Data/SpyMovementConfig.h
@@ -53,4 +53,17 @@ public:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Climbing|IK")
 	FName IKCurveFootRight = TEXT("Foot_R_IK_Weight");
+
+	// Grapple Hook
+	UPROPERTY(EditDefaultsOnly, Category = "GrappleHook")
+	float GrappleMaxRange = 3000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GrappleHook")
+	float GrappleArrivalThreshold = 150.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GrappleHook")
+	float GrappleLaunchArcZScale = 0.4f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GrappleHook")
+	float GrappleFlightTime = 0.8f;
 };
